dc9192c03461b49073d5cabd33454f35d4fbacd7
Yeguang Xue <account@yeguang-xue.com>
2024-02-03 16:32:13 -0800

    improve color mode selection

--- assets/js/color-modes.js
+++ assets/js/color-modes.js
@@ -1,48 +1,74 @@
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2023 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
-const setTheme = theme => {
-    document.documentElement.setAttribute('data-bs-theme', theme);
-
-    let btn = document.getElementById("btn_toggleDarkMode");
-    if (theme == "dark") {
-        btn.classList.add('bi-moon-stars');
-        btn.classList.remove('bi-sun');
-    } else {
-        btn.classList.remove('bi-moon-stars');
-        btn.classList.add('bi-sun');
-    }
-
-    // Need to reload Disqus when dark/light mode has been changed
-    if (typeof DISQUS !== "undefined") {
-        DISQUS.reset({ reload: true });
-    }
-}
-
-const toggleDarkMode = e => {
-    let theme = document.documentElement.getAttribute('data-bs-theme');
-    // console.log('Theme: ' + theme + " chainging theme");
-    if (theme == "dark") {
-        setTheme("light");
-    } else {
-        setTheme("dark");
-    }
-}
-
-
 (() => {
     'use strict'
-    window.addEventListener('DOMContentLoaded', () => {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            // console.log("System dark theme detected");
-            let theme = document.documentElement.getAttribute('data-bs-theme');
-            if (theme == "light") {
-                setTheme("dark");
-            }
+
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+
+        // Need to reload Disqus when dark/light mode is changed
+        if (typeof DISQUS !== "undefined") {
+            DISQUS.reset({ reload: true });
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+        if (!themeSwitcher) {
+            return
+        }
+
+        const activeThemeIcon = document.querySelector('.theme-icon-active')
+        if (theme == "dark") {
+            activeThemeIcon.className = "bi bi-moon-stars theme-icon-active"
+        }
+        else if (theme == "light") {
+            activeThemeIcon.className = "bi bi-sun theme-icon-active"
+        }
+        else if (theme == "auto") {
+            activeThemeIcon.className = "bi bi-circle-half theme-icon-active"
+        }
+
+        themeSwitcher.setAttribute('aria-label', 'toggle theme ' + theme);
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
         }
     })
-})()
 
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})()
